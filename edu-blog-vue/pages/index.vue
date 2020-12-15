743744a3bf1bd5aa9c5a122644160f3c1979c706
Noora Suurnäkki-Vogler <noora@liip.ch>
2020-12-15 10:17:01 +0100

    Attempt with multipage (help needed)

--- edu-blog-vue/pages/index.vue
+++ edu-blog-vue/pages/index.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="container">
-    <Header :image="header.image" :alt="header.alt" />
+    <Header :image="post.header.image" :alt="post.header.alt" />
     <article>
-      <time pubdate :datetime="date" :title="date">{{ date }}</time>
-      <h1>{{ title }}</h1>
+      <time pubdate :datetime="date" :title="date">{{ post.date }}</time>
+      <h1>{{ post.title }}</h1>
       <p class="lead">
         {{ lead }}
       </p>
-      <template v-for="item in content">
+      <template v-for="item in post.content">
         <p v-if="item.type === 'paragraph'" :key="item.id">
           {{ item.text }}
         </p>
@@ -45,15 +45,20 @@
           </div>
         </div>
       </template>
-      <Tag v-for="tag in tags" :key="tag.id" :text="tag.text" :href="tag.url" />
+      <Tag
+        v-for="tag in post.tags"
+        :key="tag.id"
+        :text="tag.text"
+        :href="tag.url"
+      />
     </article>
     <AuthorBio
-      :image="author.image"
-      :alt="author.alt"
-      :url="author.url"
-      :target="author.target"
-      :name="author.name"
-      :text="author.text"
+      :image="post.author.image"
+      :alt="post.author.alt"
+      :url="post.author.url"
+      :target="post.author.target"
+      :name="post.author.name"
+      :text="post.author.text"
     />
     <Footer text="Copyright 2020 Noppe" />
   </div>
@@ -65,6 +70,8 @@ import Tag from '~/components/Tag.vue'
 import AuthorBio from '~/components/AuthorBio.vue'
 import Footer from '~/components/Footer.vue'
 import '~/components/_global.css'
+import BlogPost1 from '~/data/blogpost-1.json'
+import BlogPost2 from '~/data/blogpost-2.json'
 
 export default {
   components: {
@@ -73,104 +80,16 @@ export default {
     AuthorBio,
     Footer,
   },
-  data() {
-    return {
-      header: {
-        image:
-          'https://images.unsplash.com/photo-1546074177-ffdda98d214f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80',
-        alt: 'Header image with letter tiles that says Read More',
-      },
-      title: 'Blog Post Title h1',
-      date: '28. August 2020',
-      lead:
-        'Lead Paragraph – Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-      author: {
-        image: 'https://avatars2.githubusercontent.com/u/1894309?s=120&v=4',
-        alt: 'Author portrait',
-        url: 'https://github.com/Nopps',
-        target: '_blank',
-        name: 'Noora S.',
-        text:
-          'Author bio – Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-      },
-      content: [
-        {
-          id: '1',
-          type: 'heading2',
-          text: 'Subtitle h2',
-        },
-        {
-          id: '2',
-          type: 'paragraph',
-          text:
-            'Paragraph – Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        },
-        {
-          id: '3',
-          type: 'heading3',
-          text: 'Subtitle h3',
-        },
-        {
-          id: '4',
-          type: 'cols',
-          col: [
-            {
-              type: 'ul',
-              li: [
-                'Lorem ipsum dolor sit amet',
-                'Consectetur adipiscing elit',
-                'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
-              ],
-            },
-            {
-              type: 'ol',
-              li: [
-                'Duis aute irure dolor in reprehenderit in voluptate',
-                'Quis nostrud exercitation ullamco laboris',
-                'Excepteur sint occaecat cupidatat non proident',
-              ],
-            },
-          ],
-        },
-        {
-          id: '5',
-          type: 'heading4',
-          text: 'Subtitle h4',
-        },
-        {
-          id: '6',
-          type: 'ul',
-          li: [
-            'Lorem ipsum dolor sit amet',
-            'Consectetur adipiscing elit',
-            'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
-          ],
-        },
-        {
-          id: '7',
-          type: 'paragraph',
-          text:
-            'Paragraph – Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        },
-      ],
-      tags: [
-        {
-          id: '1',
-          text: 'Tag 1',
-          url: '/tag1.html',
-        },
-        {
-          id: '2',
-          text: 'Tag 2',
-          url: '/tag2.html',
-        },
-        {
-          id: '3',
-          text: 'Tag 3',
-          url: '/tag3.html',
-        },
-      ],
-    }
+  computed: {
+    post() {
+      if (this.$route.params.slug === 'post1') {
+        return BlogPost1
+      }
+      return BlogPost2
+    },
+    footer() {
+      return 'Copyright ' + new Date().getFullYear() + ' Noppe'
+    },
   },
 }
 </script>
